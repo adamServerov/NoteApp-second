1f838a6969b026ec2ae6d1bca6fecf8508d5146a
Adam <serverov1992@bk.ru>
2022-02-10 07:49:13 +0300

    Commit

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/components/DefaultRadioButton.kt
@@ -0,0 +1,36 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes.components
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.RadioButton
+import androidx.compose.material.RadioButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun DefaultRadioButton(
+    text : String,
+    selected : Boolean,
+    onSelect : () -> Unit,
+) {
+    RadioButton(
+        selected = selected ,
+        onClick = onSelect,
+        colors = RadioButtonDefaults.colors(
+            selectedColor = MaterialTheme.colors.primary,
+            unselectedColor = MaterialTheme.colors.background
+        )
+    )
+    Spacer(modifier = Modifier.width(8.dp))
+
+    Text(
+        text = text
+    )
+
+
+}
+
